8fe8b7cc92a1512e5a2bf0c17d6426414f8dafb0
Erick Hanon <erickheinom@gmail.com>
2023-10-04 14:53:08 +0000

    feat: Cálculos de notas

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-document.write ("Olá,mundo!!!");
-console.log("Imprimindo no console!");
-window.alert("Texto no modal do navegador!");
+// Lógica de Programação
+// Problema: receber 3 notas de um aluno, 
+// calcular e apresentar a média e o resultado
+// se o aluno foi aprovado ou não.
+
+console.log ('Início do programa');
+
+let nota1;
+let nota2;
+let nota3;
+
+nota1 = parseInt(prompt('Digite a nota 1:'));
+nota2 = parseInt(prompt('Digite a nota 2:'));
+nota3 = parseInt(prompt('Digite a nota 3:'));
+
+
+
+// calculo da média
+media = (nota1 + nota2 + nota3) / 3;
+
+console.log ('A nota1 do aluno é:', nota1);
+console.log ('A nota2 do aluno é:', nota2);
+console.log ('A nota3 do aluno é:', nota3);
+console.log ('A média do aluno é:', media);
+console.log ('Fim do programa');
